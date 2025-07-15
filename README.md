972b0d3e107823a02f3d4174986ffbd84fc24fc6
kailermai <133531148+kailermai@users.noreply.github.com>
2025-07-15 17:41:30 +0800

    Added function to build speech balloon

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # go-cowsay-clone
+
+## This clone of cowsay was built following this [tutorial](https://flaviocopes.com/go-tutorial-cowsay/)
--- main.go
+++ main.go
@@ -1,11 +1,48 @@
 package main
 
 import (
-	"bufio" // for buffered IO operations (more efficient)
-	"fmt"   // for formatted IO like printf
-	"io"    // used to detect end of input
-	"os"    // handling standard IO and file stats
+	"bufio"   // for buffered IO operations (more efficient)
+	"fmt"     // for formatted IO like printf
+	"io"      // used to detect end of input
+	"os"      // handling standard IO and file stats
+	"strings" // for string manipulation
+	// "unicode/utf8" // to handle ASCII
 )
+
+// building the speech balloon
+func buildSpeechBalloon(lines []string, maxwidth int) string {
+	var borders []string
+	count := len(lines)
+	var ret []string
+
+	borders = []string{"/", "\\", "|", "<", ">"}
+
+	horizontalBorder := " " + strings.Repeat("_", maxwidth+2)
+
+	ret = append(ret, horizontalBorder)
+
+	if count == 1 {
+		s := fmt.Sprintf(`%s %s %s`, borders[3], lines[0], borders[4]) // < text >
+		ret = append(ret, s)
+	} else {
+		// top diagonals
+		s := fmt.Sprintf(`%s %s %s`, borders[0], lines[0], borders[1]) // / text \
+		ret = append(ret, s)
+
+		// left and right walls
+		for i := 1; i < count-1; i++ {
+			s = fmt.Sprintf(`%s %s %s`, borders[2], lines[0], borders[2]) // | text |
+			ret = append(ret, s)
+		}
+
+		// bottom diagonals
+		s = fmt.Sprintf(`%s %s %s`, borders[1], lines[0], borders[0]) // \ text /
+		ret = append(ret, s)
+	}
+
+	ret = append(ret, horizontalBorder)
+	return strings.Join(ret, "\n")
+}
 
 func main() {
 	// retrieve metadata about the standard input, and _ ignores error returned
@@ -15,23 +52,23 @@ func main() {
 	if info.Mode()&os.ModeCharDevice != 0 {
 		fmt.Println("This command is intended to work with pipes.")
 		fmt.Println("Usage: [text string] | gocowsay")
+		fmt.Println("Works with fortune too")
 		return
 	}
 
 	// create a buffered reader to read inputs more efficiently
 	reader := bufio.NewReader(os.Stdin)
-	var output []rune
+	var lines []string
 
 	for {
-		input, _, err := reader.ReadRune() // read one unicode character one at a time, _ takes the size of the return value
-		if err != nil && err == io.EOF {   // checks for error and end of input
+		line, _, err := reader.ReadLine() // read one line at a time, _ takes the size of the return value
+		if err != nil && err == io.EOF {  // checks for error and end of input
 			break
 		}
 
-		output = append(output, input)
+		lines = append(lines, string(line))
 	}
 
-	for j := 0; j < len(output); j++ {
-		fmt.Printf("%c", output[j])
-	}
+	speechBalloon := buildSpeechBalloon(lines, 15)
+	fmt.Printf(speechBalloon)
 }
